26714740ac39ff428047946cd0bdb456fb2e9acf
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2023-09-27 10:02:36 +0200

    SOF-1539 Add test to Part and Piece

--- src/model/entities/test/piece.spec.ts
+++ src/model/entities/test/piece.spec.ts
@@ -1,8 +1,8 @@
 import { PieceLifespan } from '../../enums/piece-lifespan'
 import { Piece, PieceInterface } from '../piece'
 
-describe('piece', () => {
-  describe('setExecutedAt', () => {
+describe(`${Piece.name}`, () => {
+  describe(`${Piece.prototype.setExecutedAt.name}`, () => {
     describe('piece is not an infinite Piece', () => {
       it('does not set executedAt', () => {
         const testee: Piece = new Piece({
@@ -32,7 +32,7 @@ describe('piece', () => {
     })
   })
 
-  describe('resetExecutedAt', () => {
+  describe(`${Piece.prototype.resetExecutedAt.name}`, () => {
     it('sets executedAt to zero', () => {
       const testee: Piece = new Piece({} as PieceInterface)
 
@@ -41,6 +41,46 @@ describe('piece', () => {
 
       const result: number = testee.getExecutedAt()
       expect(result).toEqual(0)
+    })
+  })
+
+  describe(`${Piece.prototype.setPartId.name}`, () => {
+    describe('Piece is planned', () => {
+      it('throws an error', () => {
+        const testee: Piece = new Piece({ isPlanned: true } as PieceInterface)
+        expect(() => testee.setPartId('somePartId')).toThrow()
+      })
+    })
+
+    describe('Piece is unplanned', () => {
+      it('updates the Part id', () => {
+        const partId: string = 'partId'
+        const testee: Piece = new Piece({partId: '', isPlanned: false } as PieceInterface)
+
+        expect(testee.getPartId()).not.toBe(partId)
+        testee.setPartId(partId)
+        expect(testee.getPartId()).toBe(partId)
+      })
+    })
+  })
+
+  describe(`${Piece.prototype.setStart.name}`, () => {
+    describe('Piece is planned', () => {
+      it('throws an error', () => {
+        const testee: Piece = new Piece({ isPlanned: true } as PieceInterface)
+        expect(() => testee.setStart(10)).toThrow()
+      })
+    })
+
+    describe('Piece is unplanned', () => {
+      it('updates the start time', () => {
+        const startTime: number = 100
+        const testee: Piece = new Piece({ start: 0, isPlanned: false } as PieceInterface)
+
+        expect(testee.getStart()).not.toBe(startTime)
+        testee.setStart(startTime)
+        expect(testee.getStart()).toBe(startTime)
+      })
     })
   })
 })
