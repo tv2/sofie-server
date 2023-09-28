5947a18a8ab7b27d44ac62b0c66a4da855f34cb3
André Steenhoff Madsen <asma@tv2.dk>
2023-09-28 09:46:42 +0200

    refactor: expands default values in test-entity-factory, and makes it more readable

--- src/model/entities/test/test-entity-factory.ts
+++ src/model/entities/test/test-entity-factory.ts
@@ -6,31 +6,50 @@ import { Piece, PieceInterface } from '../piece'
 export class TestEntityFactory {
   public static createRundown(rundownInterface: Partial<RundownInterface> = {}): Rundown {
     return new Rundown({
-      id: rundownInterface.id ?? 'rundownId' + Math.floor(Math.random()*1000),
-      name: rundownInterface.name ?? 'rundownName',
-      segments: rundownInterface.segments ?? []
+      id: 'rundownId' + Math.floor(Math.random()*1000),
+      name: 'rundownName',
+      segments: [],
+      isRundownActive: false,
+      modifiedAt: Date.now(),
+      ...rundownInterface
     } as RundownInterface)
   }
 
   public static createSegment(segmentInterface: Partial<SegmentInterface> = {}): Segment {
     return new Segment(
       {
-        id: segmentInterface.id ?? 'segmentId' + Math.floor(Math.random()*1000),
-        rundownId: segmentInterface.rundownId ?? 'rundownId'
+        id: 'segmentId' + Math.floor(Math.random()*1000),
+        rundownId: 'rundownId',
+        name: 'segmentName',
+        isNext: false,
+        isOnAir: false,
+        rank: 666,
+        parts: [],
+        ...segmentInterface
       } as SegmentInterface)
   }
 
   public static createPart(partInterface: Partial<PartInterface> = {}): Part {
     return new Part({
-      id: partInterface.id ?? 'partId' + Math.floor(Math.random()*1000),
-      segmentId: partInterface.segmentId ?? 'segmentId'
+      id: 'partId' + Math.floor(Math.random()*1000),
+      segmentId: 'segmentId',
+      rank: 666,
+      name: 'parrrrtName',
+      isNext: false,
+      isOnAir:false,
+      pieces: [],
+      ...partInterface
     } as PartInterface)
   }
 
   public static createPiece(pieceInterface: Partial<PieceInterface> = {}): Piece {
     return new Piece({
-      id: pieceInterface.id ?? 'pieceId' + Math.floor(Math.random()*1000),
-      partId: pieceInterface.partId ?? 'partId'
+      id: 'pieceId' + Math.floor(Math.random()*1000),
+      partId: 'partId',
+      name: 'pieceName',
+      duration: 420,
+      start: 0,
+      ...pieceInterface
     } as PieceInterface)
   }
 }
