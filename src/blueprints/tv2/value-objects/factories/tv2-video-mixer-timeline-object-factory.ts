a0adae1175db86277f56964c39aa3d1ff4805646
André Steenhoff Madsen <asma@tv2.dk>
2023-10-13 09:27:20 +0200

    style: moves/removes some comments. refactor: changes 'video-switcher' to 'video-mixer'.

--- src/blueprints/tv2/value-objects/factories/tv2-video-mixer-timeline-object-factory.ts
+++ src/blueprints/tv2/value-objects/factories/tv2-video-mixer-timeline-object-factory.ts
@@ -1,6 +1,6 @@
 import { Tv2DownstreamKeyer } from '../tv2-studio-blueprint-configuration'
 import { TimelineObject } from '../../../../model/entities/timeline-object'
 
-export interface Tv2VideoSwitcherTimelineObjectFactory {
+export interface Tv2VideoMixerTimelineObjectFactory {
   createDownstreamKeyerTimelineObject(downstreamKeyer: Tv2DownstreamKeyer, onAir: boolean): TimelineObject
 }
