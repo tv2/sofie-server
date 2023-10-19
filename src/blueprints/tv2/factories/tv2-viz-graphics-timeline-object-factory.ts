073428cd1a2a1c2027593f1c8a21ae34f4ebba7c
André Steenhoff Madsen <asma@tv2.dk>
2023-10-19 15:12:17 +0200

    feature: implements most, if not all, of Viz' 'createFullGraphicsTimelineObject'

--- src/blueprints/tv2/factories/tv2-viz-graphics-timeline-object-factory.ts
+++ src/blueprints/tv2/factories/tv2-viz-graphics-timeline-object-factory.ts
@@ -5,13 +5,17 @@ import {
   VizMseContinueTimelineObject,
   VizMseElementInternalTimelineObject,
   VizMseLoadAllElementsTimelineObject,
+  VizMseTransitionType,
   VizType
 } from '../../timeline-state-resolver-types/viz-types'
 import { Tv2GraphicsLayer } from '../value-objects/tv2-layers'
 import { DeviceType } from '../../../model/enums/device-type'
 import { TimelineObject } from '../../../model/entities/timeline-object'
+import { Tv2BaseGraphicTimelineObjectFactory } from './tv2-base-graphic-timeline-object-factory'
+import { Tv2GraphicsTarget } from '../value-objects/tv2-graphics-target'
+import { Tv2GraphicActionManifest } from '../value-objects/tv2-action-manifest'
 
-export class Tv2VizGraphicsTimelineObjectFactory implements Tv2GraphicsTimelineObjectFactory {
+export class Tv2VizGraphicsTimelineObjectFactory extends Tv2BaseGraphicTimelineObjectFactory implements Tv2GraphicsTimelineObjectFactory {
   public createThemeOutTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, duration: number): VizMseElementInternalTimelineObject {
     return {
       id: '',
@@ -110,5 +114,50 @@ export class Tv2VizGraphicsTimelineObjectFactory implements Tv2GraphicsTimelineO
     }
   }
 
+  public createFullGraphicsTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, manifest: Tv2GraphicActionManifest): TimelineObject {
+    return {
+      id: '',
+      enable: {
+        start: 0
+      },
+      priority: 1,
+      layer: this.getLayerNameFromGraphicTarget(Tv2GraphicsTarget.FULL),
+      content: this.createFullGraphicTimelineObjectContent(blueprintConfiguration, manifest),
+      classes: ['full']
+    }
+  }
 
+  protected createFullGraphicTimelineObjectContent(blueprintConfiguration: Tv2BlueprintConfiguration, manifest: Tv2GraphicActionManifest): object { // Todo: find/create proper return type.
+    return {
+      deviceType: DeviceType.VIZMSE,
+      type: VizType.ELEMENT_PILOT,
+      templateVcpId: manifest.userData.vcpid,
+      continueStep: -1,
+      noAutoPreloading: false,
+      channelName: this.getChannelNameFromGraphicTarget(Tv2GraphicsTarget.FULL),
+      ...this.getFullGraphicOutTransitionProperties(blueprintConfiguration)
+    }
+  }
+
+  private getFullGraphicOutTransitionProperties(blueprintConfiguration: Tv2BlueprintConfiguration): object {
+    if (!blueprintConfiguration.studio.PreventOverlayWithFull) {
+      return {}
+    }
+    return {
+      delayTakeAfterOutTransition: true,
+      outTransition: {
+        type: VizMseTransitionType.DELAY,
+        delay: blueprintConfiguration.studio.VizPilotGraphics.OutTransitionDuration
+      }
+    }
+  }
+
+  private getChannelNameFromGraphicTarget(target: Tv2GraphicsTarget): string {
+    switch (target) { // Todo: should the default case be here, throwing an 'UnexpectedCaseException'?
+      case Tv2GraphicsTarget.WALL: return 'WALL1'
+      case Tv2GraphicsTarget.OVL: return 'OVL1'
+      case Tv2GraphicsTarget.FULL:
+      case Tv2GraphicsTarget.TLF: return 'FULL1'
+    }
+  }
 }
