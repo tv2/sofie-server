b64cc3a399568e9d6c8f647d7df293bda77abe5b
André Steenhoff Madsen <asma@tv2.dk>
2023-10-19 15:12:17 +0200

    feature: implements most, if not all, of Viz' 'createFullGraphicsTimelineObject'

---
+++ src/blueprints/tv2/factories/tv2-base-graphic-timeline-object-factory.ts
@@ -0,0 +1,18 @@
+import { Tv2BlueprintConfiguration } from '../value-objects/tv2-blueprint-configuration'
+import { Tv2GraphicsLayer } from '../value-objects/tv2-layers'
+import { Tv2GraphicsTarget } from '../value-objects/tv2-graphics-target'
+import { Tv2GraphicActionManifest } from '../value-objects/tv2-action-manifest'
+
+export abstract class Tv2BaseGraphicTimelineObjectFactory {
+  protected abstract createFullGraphicTimelineObjectContent(blueprintConfiguration: Tv2BlueprintConfiguration, manifest: Tv2GraphicActionManifest): object // Todo: create/find better return type.
+
+  protected getLayerNameFromGraphicTarget(target: Tv2GraphicsTarget): string {
+    switch (target) { // Todo: should the default case be here, throwing an 'UnexpectedCaseException'?
+      case Tv2GraphicsTarget.WALL: return Tv2GraphicsLayer.GRAPHIC_WALL
+      case Tv2GraphicsTarget.OVL: return Tv2GraphicsLayer.GRAPHIC_OVERLAY_PILOT
+      case Tv2GraphicsTarget.FULL:
+      case Tv2GraphicsTarget.TLF: return Tv2GraphicsLayer.GRAPHICS_PILOT
+    }
+  }
+
+}
--- src/blueprints/tv2/value-objects/factories/tv2-graphics-timeline-object-factory.ts
+++ src/blueprints/tv2/value-objects/factories/tv2-graphics-timeline-object-factory.ts
@@ -1,5 +1,7 @@
 import { Tv2BlueprintConfiguration } from '../tv2-blueprint-configuration'
 import { TimelineObject } from '../../../../model/entities/timeline-object'
+import { Tv2GraphicActionManifest } from '../tv2-action-manifest'
+
 
 export interface Tv2GraphicsTimelineObjectFactory {
   createThemeOutTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, duration: number): TimelineObject
@@ -7,4 +9,5 @@ export interface Tv2GraphicsTimelineObjectFactory {
   createContinueGraphicsTimelineObject(duration: number): TimelineObject
   createClearGraphicsTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, duration: number): TimelineObject
   createAllOutGraphicsTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, duration: number): TimelineObject
+  createFullGraphicsTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, manifest: Tv2GraphicActionManifest): TimelineObject
 }
