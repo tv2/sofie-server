e5ce23bfdbbf3471b3fdf80e5120f75fb1ce0c72
André Steenhoff Madsen <asma@tv2.dk>
2023-10-23 10:25:48 +0200

    refactor: adds missing s's and removes enum that already was partially moved over, correcting usage.

--- src/blueprints/tv2/value-objects/tv2-layers.ts
+++ src/blueprints/tv2/value-objects/tv2-layers.ts
@@ -9,19 +9,20 @@ export enum Tv2SourceLayer {
   SELECTED_GRAPHICS_FULL = 'studio0_selected_graphicsFull',
   WALL_GRAPHIC = 'studio0_wall_graphics',
   TELEPHONE_GRAPHIC = 'studio0_graphicsTelefon',
-  PILOT_GRAPHIC = 'studio0_pilot',
-  PILOT_OVERLAY_GRAPHIC = 'studio0_pilotOverlay'
+  PILOT_GRAPHICS = 'studio0_pilot',
+  PILOT_OVERLAY_GRAPHICS = 'studio0_pilotOverlay'
 }
 
 export enum Tv2GraphicsLayer {
   GRAPHICS_ACTIONS = 'graphic_adlibs',
   GRAPHICS_PILOT = 'graphic_pilot',
-  GRAPHIC_OVERLAY_PILOT = 'graphic_overlay_pilot',
-  GRAPHIC_WALL = 'graphic_wall',
+  GRAPHICS_OVERLAY_PILOT = 'graphic_overlay_pilot',
+  GRAPHICS_WALL = 'graphic_wall',
 }
 
 export enum Tv2AtemLayer {
   PROGRAM = 'atem_me_program',
+  CLEAN = 'atem_me_clean',
   LOOKAHEAD = 'atem_aux_lookahead',
   DOWNSTREAM_KEYER = 'atem_dsk'
 }
@@ -39,28 +40,4 @@ export enum Tv2SisyfosLayer {
   PERSISTED_LEVELS = 'sisyfos_persisted_levels',
   AUDIO_BED = 'sisyfos_source_audiobed',
   RESYNCHRONIZE = 'sisyfos_resync'
-}
-
-export enum Tv2SwitcherMixEffectLayer {
-  PROGRAM = 'me_program',
-  CLEAN = 'me_clean',
-  CLEAN_USK_FULL = 'clean_usk_full',
-  CLEAN_USK_EFFECT = 'clean_usk_effect',
-  NEXT = 'me_next',
-  NEXT_JINGLE = 'me_next_jingle'
-}
-
-export enum Tv2SwitcherAuxLayer {
-  PROGRAM = 'aux_pgm',
-  CLEAN = 'aux_clean',
-  MIX_EFFECT_3 = 'aux_mix_effect_3', // AUX set by Sofie, but the M/E is uncontrolled by Sofie
-  WALL = 'aux_wall',
-  AR = 'aux_ar',
-  VIZ_OVL_IN_1 = 'aux_viz_ovl_in_1',
-  VENUE = 'aux_venue',
-  LOOKAHEAD = 'aux_lookahead',
-  DVE = 'aux_dve',
-  VIDEO_MIX_MINUS = 'aux_video_mix_minus',
-  SCREEN = 'aux_screen',
-  SERVER_LOOKAHEAD = 'aux_server_lookahead'
 }
