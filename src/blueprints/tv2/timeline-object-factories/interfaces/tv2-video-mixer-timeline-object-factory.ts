b87577d3053a074f44b0b138810fed6003b4eac2
André Steenhoff Madsen <asma@tv2.dk>
2023-10-30 11:23:49 +0100

    refactor: removes createDownstreamKeyerFullPilotTimelineObject by reworking createDownstreamKeyerTimelineObject's arguments.
    refactor: renames createUpstreamKeyerFullPilotTimelineObject to createUpstreamKeyerTimelineObject and reworks its arguments.

--- src/blueprints/tv2/timeline-object-factories/interfaces/tv2-video-mixer-timeline-object-factory.ts
+++ src/blueprints/tv2/timeline-object-factories/interfaces/tv2-video-mixer-timeline-object-factory.ts
@@ -1,13 +1,11 @@
 import { Tv2DownstreamKeyer } from '../../value-objects/tv2-studio-blueprint-configuration'
 import { TimelineObject } from '../../../../model/entities/timeline-object'
-import { Tv2BlueprintConfiguration } from '../../value-objects/tv2-blueprint-configuration'
 import { TimelineEnable } from '../../../../model/entities/timeline-enable'
 
 export interface Tv2VideoMixerTimelineObjectFactory {
   createProgramTimelineObject(sourceInput: number, enable: TimelineEnable, transition?: number, transitionSettings?: unknown): TimelineObject
   createCleanFeedTimelineObject(sourceInput: number, enable: TimelineEnable, transition?: number, transitionSettings?: unknown): TimelineObject
   createLookaheadTimelineObject(sourceInput: number, enable: TimelineEnable): TimelineObject
-  createDownstreamKeyerTimelineObject(downstreamKeyer: Tv2DownstreamKeyer, onAir: boolean): TimelineObject
-  createDownstreamKeyerFullPilotTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration): TimelineObject
-  createUpstreamKeyerFullPilotTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, start: number): TimelineObject
+  createDownstreamKeyerTimelineObject(downstreamKeyer: Tv2DownstreamKeyer, onAir: boolean, enable: TimelineEnable, priority: number): TimelineObject
+  createUpstreamKeyerTimelineObject(downstreamKeyer: Tv2DownstreamKeyer, enable: TimelineEnable): TimelineObject
 }
