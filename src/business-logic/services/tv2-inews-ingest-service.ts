dc3a814b4c0fd14cbeae27836cfdea19dee85cbc
Rasmus <raal@tv2.dk>
2023-11-01 08:22:46 +0100

    fix: changes uppercasing on iNews ingest service

--- src/business-logic/services/tv2-inews-ingest-service.ts
+++ src/business-logic/services/tv2-inews-ingest-service.ts
@@ -1,8 +1,10 @@
 import { IngestService } from './interfaces/ingest-service'
-import {HttpRequestParameters, HttpService} from './interfaces/http-service'
+import { HttpRequestParameters, HttpService } from './interfaces/http-service'
 
-export class Tv2InewsIngestService implements IngestService {
+export class Tv2INewsIngestService implements IngestService {
+
   constructor(private readonly httpService: HttpService) {}
+
   public async reloadIngestData(rundownName: string): Promise<void> {
     const domain = process.env.INEWS_DOMAIN ?? 'localhost:3007'
     const url = `http://${domain}/reloadData/${rundownName}`
