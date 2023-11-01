63c3e20828778027b25974a9ed75bc15a1bef441
Rasmus <raal@tv2.dk>
2023-11-01 08:42:19 +0100

    fix: changes URL's to follow REST and moves magic string to constant

--- src/business-logic/services/tv2-inews-ingest-service.ts
+++ src/business-logic/services/tv2-inews-ingest-service.ts
@@ -1,13 +1,15 @@
 import { IngestService } from './interfaces/ingest-service'
 import { HttpRequestParameters, HttpService } from './interfaces/http-service'
 
+const DEFAULT_URL: string = 'localhost:3007'
+
 export class Tv2INewsIngestService implements IngestService {
 
   constructor(private readonly httpService: HttpService) {}
 
   public async reloadIngestData(rundownName: string): Promise<void> {
-    const domain = process.env.INEWS_DOMAIN ?? 'localhost:3007'
-    const url = `http://${domain}/reloadData/${rundownName}`
+    const domain: string = process.env.INEWS_DOMAIN ?? DEFAULT_URL
+    const url: string = `http://${domain}/reloadData/rundowns/${rundownName}`
     const parameters: HttpRequestParameters = { url: url }
     await this.httpService.post(parameters)
   }
