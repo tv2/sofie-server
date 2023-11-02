5c4becf86c9d17ce84cb11afacbb90596747ff3f
André Steenhoff Madsen <asma@tv2.dk>
2023-11-02 13:56:02 +0100

    feature: creates method for generating ident timeline objects

--- src/blueprints/tv2/timeline-object-factories/interfaces/tv2-graphics-timeline-object-factory.ts
+++ src/blueprints/tv2/timeline-object-factories/interfaces/tv2-graphics-timeline-object-factory.ts
@@ -1,7 +1,6 @@
 import { Tv2BlueprintConfiguration } from '../../value-objects/tv2-blueprint-configuration'
 import { TimelineObject } from '../../../../model/entities/timeline-object'
-import { Tv2GraphicsActionManifest } from '../../value-objects/tv2-action-manifest'
-
+import { Tv2GraphicsData } from '../../value-objects/tv2-action-manifest-data'
 
 export interface Tv2GraphicsTimelineObjectFactory {
   createThemeOutTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, duration: number): TimelineObject
@@ -9,5 +8,6 @@ export interface Tv2GraphicsTimelineObjectFactory {
   createContinueGraphicsTimelineObject(duration: number): TimelineObject
   createClearGraphicsTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, duration: number): TimelineObject
   createAllOutGraphicsTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, duration: number): TimelineObject
-  createFullGraphicsTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, manifest: Tv2GraphicsActionManifest): TimelineObject
+  createFullGraphicsTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, graphicsData: Tv2GraphicsData): TimelineObject
+  createIdentGraphicsTimelineObject(blueprintConfiguration: Tv2BlueprintConfiguration, graphicsData: Tv2GraphicsData): TimelineObject
 }
