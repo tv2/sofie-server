918554a7d85e72fcddb8a75074db0826f8cc705d
André Steenhoff Madsen <asma@tv2.dk>
2023-11-13 13:03:13 +0100

    feature: adds deletion of rundown specific actions prior to saving newly generated ones

--- src/data-access/repositories/interfaces/action-repository.ts
+++ src/data-access/repositories/interfaces/action-repository.ts
@@ -3,4 +3,5 @@ import { Action } from '../../../model/entities/action'
 export interface ActionRepository {
   getAction(actionId: string): Promise<Action>
   saveActions(actions: Action[]): Promise<void>
+  deleteRundownActions(rundownId: string): Promise<void>
 }
