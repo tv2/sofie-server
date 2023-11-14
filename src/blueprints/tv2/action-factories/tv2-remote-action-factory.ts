782b8c4aea557e1ecf61e36fed08c0909f1f6a1f
Rasmus <raal@tv2.dk>
2023-11-14 15:13:52 +0100

    fix: adds untimed to remote action

--- src/blueprints/tv2/action-factories/tv2-remote-action-factory.ts
+++ src/blueprints/tv2/action-factories/tv2-remote-action-factory.ts
@@ -188,6 +188,7 @@ export class Tv2RemoteActionFactory {
       isOnAir: false,
       isNext: false,
       isUnsynced: false,
+      isUntimed: false,
       inTransition: {
         keepPreviousPartAliveDuration: 0,
         delayPiecesDuration: 0,
