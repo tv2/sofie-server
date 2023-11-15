ce2e88ff1b97b05fb6b0e4e8b5f1bbd6f081d923
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2023-11-15 16:35:35 +0100

    SOF-1696 Remove unused 'Content' type

--- src/blueprints/tv2/value-objects/tv2-content.ts
+++ src/blueprints/tv2/value-objects/tv2-content.ts
@@ -1,19 +1,3 @@
-// These are taking from Blueprints BaseContent
-
-export interface Tv2Content {
-  // ignoreMediaObjectStatus?: boolean
-  // ignoreBlackFrames?: boolean
-  // ignoreFreezeFrame?: boolean
-  // TODO: These values are included in Blueprints
-  // ignoreAudioFormat?: boolean
-  // sourceDuration?: number
-}
-
-export interface Tv2FileContent extends Tv2Content {
+export interface Tv2FileContent {
   fileName: string
-  // path: string
-  // mediaFlowIds?: string[]
-  // TODO: These values are included in Blueprints
-  // thumbnail?: string
-  // templateData?: Record<string, any>
 }
