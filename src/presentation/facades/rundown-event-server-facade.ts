ca56599f708202ed90b236d76a8329003b9ca200
André Steenhoff Madsen <asma@tv2.dk>
2023-11-30 11:47:21 +0100

    chore: changes presentation layer to use LoggerService

--- src/presentation/facades/rundown-event-server-facade.ts
+++ src/presentation/facades/rundown-event-server-facade.ts
@@ -1,9 +1,10 @@
 import { RundownEventServer } from '../emitters/interfaces/rundown-event-server'
 import { RundownWebSocketEventServer } from '../emitters/rundown-web-socket-event-server'
 import { EventEmitterFacade } from './event-emitter-facade'
+import { LoggerService } from '../../model/services/logger-service'
 
 export class RundownEventServerFacade {
   public static createRundownEventServer(): RundownEventServer {
-    return RundownWebSocketEventServer.getInstance(EventEmitterFacade.createRundownEventListener())
+    return RundownWebSocketEventServer.getInstance(EventEmitterFacade.createRundownEventListener(), new LoggerService())
   }
 }
