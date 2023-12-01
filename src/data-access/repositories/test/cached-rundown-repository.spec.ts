a60974fba4c31a885e195f28a5ac494bd5d9e73f
André Steenhoff Madsen <asma@tv2.dk>
2023-12-01 14:46:12 +0100

    test: fixes 2 tests failing due to null logger value

--- src/data-access/repositories/test/cached-rundown-repository.spec.ts
+++ src/data-access/repositories/test/cached-rundown-repository.spec.ts
@@ -1,11 +1,11 @@
 import { CachedRundownRepository } from '../cache/cached-rundown-repository'
 import { RundownRepository } from '../interfaces/rundown-repository'
-import { instance, mock, verify, when } from '@typestrong/ts-mockito'
+import { anyString, anything, instance, mock, verify, when } from '@typestrong/ts-mockito'
 import { Rundown, RundownInterface } from '../../../model/entities/rundown'
 import { ConsoleLogger } from '../../../console-logger'
 
-describe('cached-rundown-repository', () => {
-  describe('getRundown', () => {
+describe(CachedRundownRepository.name, () => {
+  describe(CachedRundownRepository.prototype.getRundown.name, () => {
     it('receives a RundownId returns a rundown', async () => {
       const mockRepo: RundownRepository = mock<RundownRepository>()
 
@@ -14,7 +14,7 @@ describe('cached-rundown-repository', () => {
 
       when(mockRepo.getRundown(randomRundownId)).thenReturn(Promise.resolve(randomRundown))
 
-      const testee: CachedRundownRepository = new CachedRundownRepository(instance(mockRepo), instance(mock(ConsoleLogger)))
+      const testee: CachedRundownRepository = new CachedRundownRepository(instance(mockRepo), createLogger())
 
       const result: Rundown = await testee.getRundown(randomRundownId)
       expect(result).toBe(randomRundown)
@@ -28,7 +28,7 @@ describe('cached-rundown-repository', () => {
 
       when(mockRepo.getRundown(randomRundownId)).thenReturn(Promise.resolve(randomRundown))
 
-      const testee: CachedRundownRepository = new CachedRundownRepository(instance(mockRepo), instance(mock(ConsoleLogger)))
+      const testee: CachedRundownRepository = new CachedRundownRepository(instance(mockRepo), createLogger())
 
       await testee.getRundown(randomRundownId)
       await testee.getRundown(randomRundownId)
@@ -36,4 +36,12 @@ describe('cached-rundown-repository', () => {
       verify(mockRepo.getRundown(randomRundownId)).once()
     })
   })
+
+  function createLogger(): ConsoleLogger {
+    const mockedLogger: ConsoleLogger = mock(ConsoleLogger)
+    when(mockedLogger.tag(anyString())).thenCall(() => createLogger())
+    when(mockedLogger.data(anything())).thenCall(() => createLogger())
+    when(mockedLogger.metadata(anything())).thenCall(() => createLogger())
+    return instance(mockedLogger)
+  }
 })
