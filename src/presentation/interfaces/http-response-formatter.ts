55b871b248280cdb42f79a48988cdb61d7ca60e5
André Steenhoff Madsen <asma@tv2.dk>
2023-12-07 13:21:12 +0100

    refactor: adds overload for 'formatErrorResponse', uses nullish coalescing instead of ternary, uses enum for JSend status'.

--- src/presentation/interfaces/http-response-formatter.ts
+++ src/presentation/interfaces/http-response-formatter.ts
@@ -1,7 +1,9 @@
 import { ErrorCode } from '../../model/enums/error-code'
+import { Exception } from '../../model/exceptions/exception'
 
 export interface HttpResponseFormatter {
   formatSuccessResponse(data?: object): object
   formatFailResponse(data?: object): object
   formatErrorResponse(message: string, errorCode: ErrorCode): object
+  formatErrorResponseFromException(exception: Exception): object
 }
--- src/presentation/jsend-response-formatter.ts
+++ src/presentation/jsend-response-formatter.ts
@@ -1,10 +1,17 @@
 import { HttpResponseFormatter } from './interfaces/http-response-formatter'
 import { ErrorCode } from '../model/enums/error-code'
+import { Exception } from '../model/exceptions/exception'
+
+enum JSendStatus {
+  ERROR = 'ERROR',
+  FAIL = 'FAIL',
+  SUCCESS = 'SUCCESS'
+}
 
 export class JsendResponseFormatter implements HttpResponseFormatter{
   public formatErrorResponse(message: string, errorCode: ErrorCode): object {
     return {
-      status: 'error',
+      status: JSendStatus.ERROR,
       code: errorCode,
       message: message,
     }
@@ -12,15 +19,19 @@ export class JsendResponseFormatter implements HttpResponseFormatter{
 
   public formatFailResponse(data?: object): object {
     return {
-      status: 'fail',
-      data: data ? data : null
+      status: JSendStatus.FAIL,
+      data: data ?? null
     }
   }
 
   public  formatSuccessResponse(data?: object): object {
     return {
-      status: 'success',
-      data: data ? data : null
+      status: JSendStatus.SUCCESS,
+      data: data ?? null
     }
+  }
+
+  public formatErrorResponseFromException(exception: Exception): object {
+    return this.formatErrorResponse(exception.message, exception.errorCode)
   }
 }
