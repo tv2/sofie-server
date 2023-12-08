0f8c46b0ef100c94a7062138473ae96771fdaf83
André Steenhoff Madsen <asma@tv2.dk>
2023-12-08 11:20:42 +0100

    refactor: renames enum to better match content and adds missing space.

--- src/presentation/jsend-response-formatter.ts
+++ src/presentation/jsend-response-formatter.ts
@@ -2,16 +2,16 @@ import { HttpResponseFormatter } from './interfaces/http-response-formatter'
 import { ErrorCode } from '../model/enums/error-code'
 import { Exception } from '../model/exceptions/exception'
 
-enum JSendStatus {
+enum RequestStatus {
   ERROR = 'ERROR',
   FAIL = 'FAIL',
   SUCCESS = 'SUCCESS'
 }
 
-export class JsendResponseFormatter implements HttpResponseFormatter{
+export class JsendResponseFormatter implements HttpResponseFormatter {
   public formatErrorResponse(message: string, errorCode: ErrorCode): object {
     return {
-      status: JSendStatus.ERROR,
+      status: RequestStatus.ERROR,
       code: errorCode,
       message: message,
     }
@@ -23,14 +23,14 @@ export class JsendResponseFormatter implements HttpResponseFormatter{
 
   public formatFailResponse(data?: unknown): object {
     return {
-      status: JSendStatus.FAIL,
+      status: RequestStatus.FAIL,
       data: data ?? null
     }
   }
 
   public formatSuccessResponse(data?: unknown): object {
     return {
-      status: JSendStatus.SUCCESS,
+      status: RequestStatus.SUCCESS,
       data: data ?? null
     }
   }
