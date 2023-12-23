98ae7689e64fbab7aaefc85712cb04259a8ed383
Dimitar G. Moustakinov <k-primeholding-dim@tv2.dk>
2023-12-23 18:27:55 +0200

    SOF-1754 - fixes spec

--- src/data-access/repositories/test/configuration-controller.spec.ts
+++ src/data-access/repositories/test/configuration-controller.spec.ts
@@ -1,38 +1,38 @@
-import { instance, mock, verify } from "@typestrong/ts-mockito";
-import { ConfigurationRepository } from "../interfaces/configuration-repository";
-import { ConfigurationController } from "../../../presentation/controllers/configuration-controller";
-import { Request, Response } from "express";
-import { ShowStyleVariantRepository } from "../interfaces/show-style-variant-repository";
-import { HttpErrorHandler } from "../../../presentation/interfaces/http-error-handler";
-import { HttpResponseFormatter } from "../../../presentation/interfaces/http-response-formatter";
+import { instance, mock, verify } from '@typestrong/ts-mockito'
+import { ConfigurationRepository } from '../interfaces/configuration-repository'
+import { ConfigurationController } from '../../../presentation/controllers/configuration-controller'
+import { Request, Response } from 'express'
+import { ShowStyleVariantRepository } from '../interfaces/show-style-variant-repository'
+import { HttpErrorHandler } from '../../../presentation/interfaces/http-error-handler'
+import { HttpResponseFormatter } from '../../../presentation/interfaces/http-response-formatter'
 
 describe(ConfigurationController.name, () => {
   describe(
     ConfigurationController.prototype.postClearConfigurationCache.name,
     () => {
-      it("invokes repo method for clearing of configuration cache when posted", () => {
-        const mockRequest: Request = mock<Request>();
-        const mockResponse: Response = mock<Response>();
+      it('invokes repo method for clearing of configuration cache when posted', () => {
+        const mockRequest: Request = mock<Request>()
+        const mockResponse: Response = mock<Response>()
         const mockConfigurationRepository: ConfigurationRepository =
-          mock<ConfigurationRepository>();
+          mock<ConfigurationRepository>()
         const mockShowStyleVariantRepository: ShowStyleVariantRepository =
-          mock<ShowStyleVariantRepository>();
-        const mockHttpErrorHandler: HttpErrorHandler = mock<HttpErrorHandler>();
+          mock<ShowStyleVariantRepository>()
+        const mockHttpErrorHandler: HttpErrorHandler = mock<HttpErrorHandler>()
         const mockHttpResponseFormatter: HttpResponseFormatter =
-          mock<HttpResponseFormatter>();
+          mock<HttpResponseFormatter>()
         const configurationController: ConfigurationController =
           new ConfigurationController(
             instance(mockConfigurationRepository),
             instance(mockShowStyleVariantRepository),
             instance(mockHttpErrorHandler),
             instance(mockHttpResponseFormatter),
-          );
+          )
         configurationController.postClearConfigurationCache(
           mockRequest,
           mockResponse,
-        );
-        verify(mockConfigurationRepository.clearConfigurationCache()).once();
-      });
+        )
+        verify(mockConfigurationRepository.clearConfigurationCache()).once()
+      })
     },
-  );
-});
+  )
+})
