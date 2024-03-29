50b7ad763a355c8d913d323e051316b231486507
Dimitar G. Mustakinov <k-primeholding-dim@tv2.dk>
2024-03-29 19:49:15 +0200

    SOF-1596 - fix: simplify all

--- db/manager/mongo.ts
+++ db/manager/mongo.ts
@@ -1,5 +1,5 @@
 import { parseArgs } from 'node:util'
-import {exec} from 'node:child_process'
+import { exec } from 'node:child_process'
 import { MongoClient } from 'mongodb'
 
 const {
@@ -60,34 +60,32 @@ if (values.help) {
   console.log('  --seed        Seed the database with initial data')
   console.log('  --drop        Stop then drop the database container and its volumes')
   console.log('  --spy         Spy on database events')
-  console.log('  --dump        Dump all from mongodb://127.0.0.1:3001/ to ./db/dumps/meteor')
+  console.log('  --dump        Dump all from mongodb://gateway.docker.internal:3001/ to ./db/dumps/meteor')
 
   process.exit(0)
 }
 
-function docker(args: string[]): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const child = exec(args.join(' '))
-    child.on('error', (err) => {
-      reject(err)
-    })
-    child.on('exit', (code) => {
-      if (code === 0) {
-        resolve()
-      } else {
-        reject(code)
+function docker(args: string[]): void {
+  if (args.length === 0) {
+    throw new Error('No arguments provided to docker command')
+  }
+
+  exec(
+    ['docker']
+      .concat(args)
+      .join(' '),
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(stderr)
+        process.exit(error.code || 1)
       }
-    })
-  })
+      console.log(stdout)
+    }
+  )
 }
 
-async function testDockerIsInstalled(): Promise<void> {
-  return docker(['docker', '--version'])
-}
-
-async function initReplicaSet(): Promise<void> {
-  return docker([
-    'docker',
+function initReplicaSet(): void {
+  docker([
     'exec',
     'sofie-mongodb',
     'mongosh',
@@ -98,9 +96,8 @@ async function initReplicaSet(): Promise<void> {
   ])
 }
 
-async function startMongoContainer(): Promise<void> {
-  return docker([
-    'docker',
+function startMongoContainer(): void {
+  docker([
     'run',
     '--name', 'sofie-mongodb',
     '-v', './db/dumps:/dumps',
@@ -116,45 +113,41 @@ async function startMongoContainer(): Promise<void> {
   ])
 }
 
-async function stopMongoContainer(): Promise<void> {
-  return docker([
-    'docker',
+function stopMongoContainer(): void {
+  docker([
     'stop',
     'sofie-mongodb',
     '-t', '2'
   ])
 }
 
-async function restartMongoContainer(): Promise<void> {
-  return docker([
-    'docker',
+function restartMongoContainer(): void {
+  docker([
     'restart',
     'sofie-mongodb',
     '-t','2'
   ])
 }
 
-async function dropMongoContainer(): Promise<void> {
-  return docker([
-    'docker',
+function dropMongoContainer(): void {
+  docker([
     'rm',
     '-f', '-v',
     'sofie-mongodb',
   ])
 }
 
-async function spyOnDatabase(): Promise<void>{
-  const client = new MongoClient('mongodb://localhost:3001/sofie?replicaSet=rs0')
-  return client.connect().then( (client) => {
-    const changeStream = client.watch()
-    changeStream.on('change', (change) => console.dir(change))
-  })
+function spyOnDatabase(): void {
+  void new MongoClient('mongodb://localhost:3001/sofie?replicaSet=rs0')
+    .connect().then( (client) => {
+      const changeStream = client.watch()
+      changeStream.on('change', (change) => console.dir(change))
+    })
 }
 
-async function seedDatabase(): Promise<void>{
+function seedDatabase(): void {
   // Seed the database with content from ./db/dumps/meteor
-  return docker([
-    'docker',
+  docker([
     'exec',
     'sofie-mongodb',
     'mongorestore',
@@ -164,18 +157,16 @@ async function seedDatabase(): Promise<void>{
   ])
 }
 
-async function dumptDatabase(): Promise<void>{
-  const args = [
-    'docker', 'run', '--rm', '-it' ,
+function dumptDatabase(): void {
+  docker([
+    'run', '--rm', '-it' ,
     '-v', './db/dumps:/dumps',
     'leafney/alpine-mongo-tools:latest',
     'mongodump', '--host=gateway.docker.internal', '--port=3001', '--oplog', '--out=/dumps/meteor'
-  ]
-  console.log(args.join(' '))
-  return docker(args)
+  ])
 }
 
-async function action(values: { [longOption: string]: string | boolean | undefined } & {
+function action(values: { [longOption: string]: string | boolean | undefined } & {
   help: string | boolean | undefined
   start: string | boolean | undefined
   stop: string | boolean | undefined
@@ -185,54 +176,46 @@ async function action(values: { [longOption: string]: string | boolean | undefin
   drop: string | boolean | undefined
   spy: string | boolean | undefined
   dump: string | boolean | undefined
-}): Promise<void> {
+}): void {
   switch (true) {
     case values.start && !values.stop && !values.restart && !values.initrs && !values.seed && !values.drop && !values.spy && !values.dump:
       console.log('starting mongo...')
-      await testDockerIsInstalled()
-      await startMongoContainer()
+      startMongoContainer()
       break
 
     case values.stop && !values.start && !values.restart && !values.initrs && !values.seed && !values.drop && !values.spy && !values.dump:
       console.log('stopping mongo...')
-      await testDockerIsInstalled()
-      await stopMongoContainer()
+      stopMongoContainer()
       break
 
     case values.restart && !values.start && !values.stop && !values.initrs && !values.seed && !values.drop && !values.spy && !values.dump:
       console.log('restarting mongo...')
-      await testDockerIsInstalled()
-      await restartMongoContainer()
+      restartMongoContainer()
       break
 
     case values.initrs && !values.start && !values.stop && !values.restart && !values.seed && !values.drop && !values.spy && !values.dump:
       console.log('initializing replica set...')
-      await testDockerIsInstalled()
-      await initReplicaSet()
+      initReplicaSet()
       break
 
     case values.seed && !values.start && !values.stop && !values.restart && !values.initrs && !values.drop && !values.spy && !values.dump:
       console.log('seeding database...')
-      await testDockerIsInstalled()
-      await seedDatabase()
+      seedDatabase()
       break
 
     case values.drop && !values.start && !values.stop && !values.restart && !values.initrs && !values.seed && !values.spy && !values.dump:
       console.log('dropping database...')
-      await testDockerIsInstalled()
-      await dropMongoContainer()
+      dropMongoContainer()
       break
 
     case values.spy && !values.start && !values.stop && !values.restart && !values.initrs && !values.seed && !values.drop && !values.dump:
       console.log('spying on the database...')
-      await testDockerIsInstalled()
-      await spyOnDatabase()
+      spyOnDatabase()
       break
 
     case values.dump && !values.start && !values.stop && !values.restart && !values.initrs && !values.seed && !values.drop && !values.spy:
       console.log('dumping database...')
-      await testDockerIsInstalled()
-      await dumptDatabase()
+      dumptDatabase()
       break
 
     default:
@@ -242,18 +225,4 @@ async function action(values: { [longOption: string]: string | boolean | undefin
   }
 }
 
-function translateCode(e: string): string {
-  switch (true){
-    case (e === '125'):
-      return 'There is already a running container.'
-  }
-
-  return `Unknown: ${e}`
-}
-
-action(values).then(
-  () => {},
-  (e) => {
-    console.error(translateCode(e.toString()))
-  }
-).catch( e => console.error(translateCode(e)))
+action(values)
