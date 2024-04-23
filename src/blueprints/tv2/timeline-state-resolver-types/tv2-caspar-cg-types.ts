fc9c143cc468a9292f0d3af0a339af2707e7af54
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2024-04-23 07:51:12 +0200

    SOF-1712 Move Tv2 types for CasparCG out of the 'generic' TSR types folder

---
+++ src/blueprints/tv2/timeline-state-resolver-types/tv2-caspar-cg-types.ts
@@ -0,0 +1,48 @@
+export interface Tv2CasparCgTemplateData {
+  display: Tv2CasparCgTemplateDisplayMode
+  partialUpdate: boolean
+  slots: {
+    [Tv2CasparCgTemplateSlotType.FULLSCREEN_GRAPHICS]?: {
+      display: Tv2CasparCgTemplateDisplayMode
+      payload: {
+        type: 'still'
+        url: string,
+        noAnimation: boolean
+      }
+    }
+    [Tv2CasparCgTemplateSlotType.LOWER_THIRD]?: {
+      display: Tv2CasparCgTemplateDisplayMode
+      payload: {
+        type: string
+        0: string
+      }
+    }
+    [Tv2CasparCgTemplateSlotType.IDENT]?: {
+      display: Tv2CasparCgTemplateDisplayMode
+      payload: {
+        type: string
+        0: string
+      }
+    }
+    [Tv2CasparCgTemplateSlotType.SPLIT_SCREEN]?: {
+      display: Tv2CasparCgTemplateDisplayMode
+      payload: {
+        type: 'locators'
+        style: object
+      }
+    }
+  }
+}
+
+export enum Tv2CasparCgTemplateSlotType {
+  FULLSCREEN_GRAPHICS = '250_full',
+  LOWER_THIRD = '450_lowerThird',
+  IDENT = '650_ident',
+  SPLIT_SCREEN = '850_dve',
+}
+
+export enum Tv2CasparCgTemplateDisplayMode {
+  PROGRAM = 'program',
+  PREVIEW = 'preview',
+  HIDDEN = 'hidden',
+}
