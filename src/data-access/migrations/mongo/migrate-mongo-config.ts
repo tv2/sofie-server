cdb875547a362f403a31b6b2a5de8094a5225532
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2024-05-14 15:07:36 +0200

    Remove test migrations. Add migration template

--- src/data-access/migrations/mongo/migrate-mongo-config.ts
+++ src/data-access/migrations/mongo/migrate-mongo-config.ts
@@ -23,7 +23,7 @@ const config = {
   },
 
   // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
-  migrationsDir: 'src/data-access/migrations/mongo/migrations',
+  migrationsDir: 'src/data-access/migrations/mongo/mongo-migrations',
 
   // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
   changelogCollectionName: 'changelog',
