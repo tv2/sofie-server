e7ca7525e37ad195d3fd2abaa25aac72a9fb5f94
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2024-05-21 07:53:56 +0200

    update yarn script names

--- src/data-access/migrations/mongo/migrate-mongo-config.ts
+++ src/data-access/migrations/mongo/migrate-mongo-config.ts
@@ -13,13 +13,6 @@ const config = {
     url: MONGO_CONNECTION_STRING,
 
     databaseName: MONGO_DB_NAME,
-
-    options: {
-      // useNewUrlParser: true, // removes a deprecation warning when connecting
-      // useUnifiedTopology: true, // removes a deprecating warning when connecting
-      //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
-      //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
-    }
   },
 
   // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
