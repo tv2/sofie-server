20d9fc9afa9214143380922dd71f9bf89d83fce7
Edward Forestier <edfo@tv2.dk>
2024-07-15 15:13:55 +0200

    SOF-1964 fixed latest PR comments

--- src/data-access/repositories/mongo/mongo-device-repository.ts
+++ src/data-access/repositories/mongo/mongo-device-repository.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { BaseMongoRepository } from './base-mongo-repository'
-import { DeviceRepository } from '../interfaces/device-repository'
-import { Device } from '../../../model/entities/device'
-import { MongoDatabase } from './mongo-database'
-import { UuidGenerator } from '../interfaces/uuid-generator'
-import { NotFoundException } from '../../../model/exceptions/not-found-exception'
+import {BaseMongoRepository} from './base-mongo-repository'
+import {DeviceRepository} from '../interfaces/device-repository'
+import {Device} from '../../../model/entities/device'
+import {MongoDatabase} from './mongo-database'
+import {UuidGenerator} from '../interfaces/uuid-generator'
+import {NotFoundException} from '../../../model/exceptions/not-found-exception'
 
 
 const DEVICE_COLLECTION_NAME: string = 'externalDevices'
@@ -21,15 +21,15 @@ export class MongoDeviceRepository extends BaseMongoRepository implements Device
 
   public async getDevices(): Promise<Device[]> {
     this.assertDatabaseConnection(MongoDeviceRepository.prototype.getDevices.name)
-    return await this.getCollection().find<Device>({}).toArray() 
+    return await this.getCollection().find<Device>({}).toArray()
   }
 
   public async getDevice(deviceId: string): Promise<Device> {
     this.assertDatabaseConnection(MongoDeviceRepository.prototype.getDevice.name)
-    
-    const device: Device | null = await this.getCollection().findOne<Device>({ id: deviceId })
+
+    const device: Device | null = await this.getCollection().findOne<Device>({id: deviceId})
     if (!device) {
-      throw new NotFoundException('') // TODO: Better error message
+      throw new NotFoundException(`Unable to find device with id '${deviceId}'.`)
     }
     return device
   }
@@ -40,6 +40,6 @@ export class MongoDeviceRepository extends BaseMongoRepository implements Device
       device.id = this.uuidGenerator.generateUuid() // TODO: Remove side effect
     }
 
-    await this.getCollection().updateOne({ id: device.id }, { $set: device }, { upsert: true })
+    await this.getCollection().updateOne({id: device.id}, {$set: device}, {upsert: true})
   }
 }
