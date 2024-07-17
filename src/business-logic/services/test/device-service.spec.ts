058114bf71978f5334b09dad5f393c6b0c89d3d2
Edward <edfo@tv2.dk>
2024-07-17 12:45:19 +0200

    SOF-1955 updated tests

--- src/business-logic/services/test/device-service.spec.ts
+++ src/business-logic/services/test/device-service.spec.ts
@@ -1,41 +1,69 @@
-import {DeviceServiceImplementation} from '../device-service-implementation'
-import {Device, TelemetricsDevice} from '../../../model/entities/device'
-import {instance, mock, verify, when} from '@typestrong/ts-mockito'
-import {MongoDeviceRepository} from '../../../data-access/repositories/mongo/mongo-device-repository'
-import {DeviceService} from '../interfaces/device-service'
-import {DeviceEventEmitter} from '../interfaces/device-event-emitter'
+import { DeviceServiceImplementation } from '../device-service-implementation'
+import { Device } from '../../../model/entities/device'
+import { instance, mock, verify, when } from '@typestrong/ts-mockito'
+import { DeviceEventEmitter } from '../interfaces/device-event-emitter'
+import { DeviceRepository } from '../../../data-access/repositories/interfaces/device-repository'
+import { EntityTestFactory } from '../../../model/entities/test/entity-test-factory'
 
-describe('DeviceService', () => {
-  let testDevice: TelemetricsDevice
-  let deviceRepository: MongoDeviceRepository
-  let deviceEventEmitter: DeviceEventEmitter
-  const mockedValue: MongoDeviceRepository = mock<MongoDeviceRepository>()
+describe(DeviceServiceImplementation.name, () => {
+  describe(DeviceServiceImplementation.prototype.getDevices.name, () => {
+    it('should get all devices', async () => {
+      const devices: Device[] = [EntityTestFactory.createDevice()]
+      const deviceRepository: DeviceRepository = mock<DeviceRepository>()
+      when(deviceRepository.getDevices()).thenReturn(Promise.resolve(devices))
+      const testee: DeviceServiceImplementation = new DeviceServiceImplementation(instance(deviceRepository), instance(mock<DeviceEventEmitter>()))
 
-  beforeEach(() => {
-    testDevice = mock<TelemetricsDevice>()
-    deviceRepository = instance(mockedValue)
-    deviceEventEmitter = mock<DeviceEventEmitter>()
+      const result: Device[] = await testee.getDevices()
+
+      expect(result).toEqual(devices)
+    })
   })
 
+  describe(DeviceServiceImplementation.prototype.getDevice.name, () => {
+    it('should get one specific device', async () => {
+      const deviceToBeFetched: Device = EntityTestFactory.createDevice({id: 'new-device'})
+      const deviceRepository: DeviceRepository = mock<DeviceRepository>()
+      when(deviceRepository.getDevice(deviceToBeFetched.id)).thenReturn(Promise.resolve(deviceToBeFetched))
+      const testee: DeviceServiceImplementation = createTestee({deviceRepository: instance(deviceRepository)})
 
-  it('should read all configurations', async () => {
-    const devices: Device[] = [testDevice]
-    const mockedService: DeviceService = mock<DeviceService>()
-    const testee: DeviceServiceImplementation = new DeviceServiceImplementation(deviceRepository, deviceEventEmitter)
-    when(mockedService.getDevices()).thenReturn(Promise.resolve(devices))
+      const result: Device = await testee.getDevice(deviceToBeFetched.id)
 
-    await testee.getDevices()
-
-    verify(mockedValue.getDevices()).called()
+      expect(result).toEqual(deviceToBeFetched)
+    })
   })
 
-  it('should create a new configuration', async () => {
-    const mockedService: DeviceService = mock<DeviceService>()
-    const testee: DeviceServiceImplementation = new DeviceServiceImplementation(deviceRepository, deviceEventEmitter)
-    when(mockedService.create(testDevice)).thenReturn(Promise.resolve())
+  describe(DeviceServiceImplementation.prototype.create.name, () => {
+    it('should create a new device', async () => {
+      const deviceToBeCreated: Device = EntityTestFactory.createDevice({id: 'new-device'})
+      const deviceRepository: DeviceRepository = mock<DeviceRepository>()
+      when(deviceRepository.save(deviceToBeCreated))
+      const testee: DeviceServiceImplementation = createTestee({deviceRepository: instance(deviceRepository)})
 
-    await testee.create(testDevice)
+      await testee.create(deviceToBeCreated)
 
-    verify(mockedValue.save(testDevice)).called()
+      verify(deviceRepository.save(deviceToBeCreated)).called()
+    })
+
+    it('should emit a DeviceCreatedEvent', async () => {
+      const deviceToBeCreated: Device = EntityTestFactory.createDevice({id: 'new-device'})
+      const deviceEventEmitter: DeviceEventEmitter = mock<DeviceEventEmitter>()
+      const testee: DeviceServiceImplementation = createTestee({deviceEventEmitter: instance(deviceEventEmitter)})
+
+      await testee.create(deviceToBeCreated)
+
+      verify(deviceEventEmitter.emitDeviceCreatedEvent(deviceToBeCreated)).once()
+    })
   })
 })
+
+function createTestee(params?: {
+  deviceRepository?: DeviceRepository,
+  deviceEventEmitter?: DeviceEventEmitter
+}): DeviceServiceImplementation {
+  return new DeviceServiceImplementation(
+    params?.deviceRepository ?? instance(mock<DeviceRepository>()),
+    params?.deviceEventEmitter ?? mock<DeviceEventEmitter>(),
+  )
+}
+
+
