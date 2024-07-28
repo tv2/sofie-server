1a26bd4a6d938be399fb2e595c3ffa7a4cc5b9ac
Edward <edfo@tv2.dk>
2024-07-28 23:22:06 +0200

    SOF-1965 connection logic for inews

--- src/business-logic/services/inews-gateway-connection-implementation.ts
+++ src/business-logic/services/inews-gateway-connection-implementation.ts
@@ -1,8 +1,13 @@
+import { WebSocket } from 'ws'
 import { Device } from '../../model/entities/device'
 import { DeviceConnection } from './interfaces/inewsgateway-device-connection'
 
 export class INewsGatewayDeviceConnection implements DeviceConnection {
   private readonly device: Device
+  private readonly INEWS_GATEWAY_HOST: string = process.env.INEWS_GATEWAY_HOST ?? 'localhost:3008'
+
+  private client: WebSocket
+  private pingTimeout: NodeJS.Timeout
   private isConnectingOrDisconnecting: boolean = false
 
   constructor(_device: Device) {
@@ -10,6 +15,7 @@ export class INewsGatewayDeviceConnection implements DeviceConnection {
   }
 
   public async send(_deviceId: string, _params: string[]): Promise<void> {
+    this.client.send(JSON.stringify(_params[0]))
     // For now: Simulate an async operation, e.g., network request
     await new Promise(resolve => setTimeout(resolve, 1000))
   }
@@ -20,45 +26,99 @@ export class INewsGatewayDeviceConnection implements DeviceConnection {
   }
 
   public async connect(): Promise<boolean> {
-    if (this.isConnectingOrDisconnecting) {
-      throw new Error('Device is already connecting or disconnecting.')
-    }
-    this.isConnectingOrDisconnecting = true
+    return new Promise<boolean>((resolve, reject) => {
+      if (this.isConnectingOrDisconnecting) {
+        return reject(new Error('Device is already connecting or disconnecting.'))
+      }
 
-    try {
-      // Implement connection logic here
+      try {
+        this.client = new WebSocket(this.INEWS_GATEWAY_HOST)
+        this.initializeClientEvents()
+        this.device.isConnected = true
+        resolve(true)
+  
+      } catch (error) {
+        this.device.isConnected = false
+        reject(new Error('WebSocket connection failed: ' + error))
+        return false
+      } finally {
+        this.isConnectingOrDisconnecting = false
+      }
+    })
+  }
 
-      // For now: Simulate an async operation, e.g., network request
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      this.device.isConnected = true
-      return true
-    } catch (error) {
+  private initializeClientEvents(): boolean {
+    this.client.on('error', (error) => {
+      console.error('WebSocket error:', error)
       this.device.isConnected = false
-      return false
-    } finally {
-      this.isConnectingOrDisconnecting = false
+      // use logger instead to log reason for closing
+    })
+  
+    this.client.on('open', () => {
+      this.heartbeat()
+    })
+
+    this.client.on('ping', () => {
+      this.heartbeat()
+    })
+
+    this.client.on('close', (code, reason) => {
+      if (this.pingTimeout !== null) {
+        clearTimeout(this.pingTimeout)
+      }
+
+      if (this.device.isConnected) {
+        this.device.isConnected = false
+      }
+      // use logger instead to log reason for closing
+      console.log(`WebSocket closed: Code ${code}, Reason: ${reason}`)
+
+      // reconnection logic
+    })
+
+    return this.device.isConnected
+  }
+
+  private heartbeat(): void {
+    if (this.pingTimeout !== null) {
+      clearTimeout(this.pingTimeout)
     }
+
+    this.pingTimeout = setTimeout(() => {
+      this.terminate()
+    }, 30000 + 1000)
+  }
+
+  private terminate(): void {
+    if (this.client.readyState === WebSocket.OPEN) {
+      this.client.terminate()
+    }  
   }
 
   public async disconnect(): Promise<boolean> {
-    if (this.isConnectingOrDisconnecting) {
-      throw new Error('Device is already connecting or disconnecting.')
-    }
-    this.isConnectingOrDisconnecting = true
+    return new Promise<boolean>((_resolve, reject) => { 
+      if (this.isConnectingOrDisconnecting) {
+        return reject(new Error('Device is already connecting or disconnecting.'))
+      }
+      this.isConnectingOrDisconnecting = true
 
-    try {
-      // Implement disconnection logic here
-
-      // For now: Simulate an async operation, e.g., network request
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      this.device.isConnected = false
-      return true
-    } catch (error) {
-      this.device.isConnected = true
-      return false
-    } finally {
-      this.isConnectingOrDisconnecting = false
-    }
+      try {
+        if (this.client.readyState === WebSocket.OPEN) {
+          this.client.close()
+        }
+    
+        if (this.pingTimeout !== null) {
+          clearTimeout(this.pingTimeout)
+        }      
+        this.device.isConnected = false
+        return true
+      } catch (error) {
+        this.device.isConnected = true
+        return false
+      } finally {
+        this.isConnectingOrDisconnecting = false
+      }
+    })
   }
 }
   
