1e3fdfa9eb1d14940bb48fb8464ac0a80867be90
Anders Frederik Jørgensen <afjo@tv2.dk>
2024-07-30 13:13:14 +0200

    fix: Adds Piece.isInsertedOnAir to fix the calculation for piece start delays.

--- src/data-access/repositories/mongo/mongo-entity-converter.ts
+++ src/data-access/repositories/mongo/mongo-entity-converter.ts
@@ -124,6 +124,7 @@ export interface MongoPiece extends MongoId {
   content?: unknown
   tags: string[]
   isUnsynced: boolean
+  isInsertedOnAir?: boolean
 }
 
 export interface MongoTimeline extends MongoId {
@@ -374,7 +375,8 @@ export class MongoEntityConverter {
   public convertToPiece(mongoPiece: MongoPiece): Piece {
     return new Piece({
       ...mongoPiece,
-      id: mongoPiece._id
+      id: mongoPiece._id,
+      isInsertedOnAir: mongoPiece.isInsertedOnAir,
     })
   }
 
@@ -400,7 +402,8 @@ export class MongoEntityConverter {
       metadata: piece.metadata,
       content: piece.content,
       isUnsynced: piece.isUnsynced(),
-      tags: piece.tags
+      tags: piece.tags,
+      isInsertedOnAir: piece.isInsertedOnAir(),
     }
   }
 
