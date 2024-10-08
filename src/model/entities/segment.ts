f6bea58c68f94ff181179669077ab7daaa9f7620
Anders Frederik Lou Jørgensen <afjo@tv2.dk>
2024-10-08 10:25:06 +0200

    chore: Adds more information to part already exists exception message when trying to add part to segment.

--- src/model/entities/segment.ts
+++ src/model/entities/segment.ts
@@ -162,7 +162,7 @@ export class Segment {
 
     const doesPartAlreadyExistOnSegment: boolean = this.parts.some(part => part.id === partToAdd.id)
     if (doesPartAlreadyExistOnSegment) {
-      throw new AlreadyExistException(`Unable to add Part to Segment. Part ${partToAdd.id} already exist on Segment ${this.id}`)
+      throw new AlreadyExistException(`Unable to add the part '${partToAdd.name}' with id '${partToAdd.id}' to the segment '${this.name}' with id '${this.id}'. The part already exists on the segment.`)
     }
     this.parts.push(partToAdd)
     this.parts.sort(this.compareParts)
