de5d758e834469cb07aff2c6b5706613bfb91b89
Anders Frederik Lou Jørgensen <afjo@tv2.dk>
2024-10-16 07:37:17 +0200

    refactor: RundownAggregateRepository interface is converted to a type declaration.

--- src/data-access/repositories/interfaces/rundown-aggregate-repository.ts
+++ src/data-access/repositories/interfaces/rundown-aggregate-repository.ts
@@ -2,4 +2,4 @@ import { RundownRepository } from './rundown-repository'
 import { SegmentRepository } from './segment-repository'
 import { PartRepository } from './part-repository'
 
-export interface RundownAggregateRepository extends RundownRepository, SegmentRepository, PartRepository {}
+export type RundownAggregateRepository = RundownRepository & SegmentRepository & PartRepository
