c62837956731302cfcece8debba31857ab7d3f30
Anders Frederik Lou Jørgensen <afjo@tv2.dk>
2024-10-22 12:33:50 +0200

    refactor: Makes RundownAggregateRepository into an interface.

--- src/data-access/repositories/interfaces/rundown-aggregate-repository.ts
+++ src/data-access/repositories/interfaces/rundown-aggregate-repository.ts
@@ -3,4 +3,4 @@ import { SegmentRepository } from './segment-repository'
 import { PartRepository } from './part-repository'
 import { PieceRepository } from './piece-repository'
 
-export type RundownAggregateRepository = RundownRepository & SegmentRepository & PartRepository & PieceRepository
+export interface RundownAggregateRepository extends RundownRepository, SegmentRepository, PartRepository, PieceRepository {}
