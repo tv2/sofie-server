685ceeb7517c28ca851a6fc14b22c5f8ee8f6ebc
Anders Frederik Lou Jørgensen <afjo@tv2.dk>
2024-11-26 12:22:50 +0100

    test: Use EntityTestFactory.createRundownInterface instead of 'as RundownInterface'.

--- src/model/entities/test/rundown.spec.ts
+++ src/model/entities/test/rundown.spec.ts
@@ -4865,12 +4865,19 @@ describe(Rundown.name, () => {
           const segment: Segment = EntityTestFactory.createSegment({ parts: [onAirPart, unplannedPartOneAfterActivePart, unplannedPartTwoAfterActivePart] })
           const threshold: number = 1
 
-          const testee: Rundown = new Rundown({ segments: [segment], mode: RundownMode.ACTIVE, alreadyActiveProperties: {
-            activeCursor: {
-              segment,
-              part: onAirPart
+          const testee: Rundown = new Rundown(EntityTestFactory.createRundownInterface({
+            segments: [segment],
+            mode: RundownMode.ACTIVE,
+            alreadyActiveProperties: {
+              activeCursor: {
+                segment,
+                part: onAirPart,
+                owner: Owner.SYSTEM,
+              },
+              nextCursor: undefined,
+              infinitePieces: new Map(),
             }
-          } } as RundownInterface)
+          }))
 
           expect(segment.getParts()).toContain(onAirPart)
           expect(segment.getParts()).toContain(unplannedPartOneAfterActivePart)
@@ -4938,12 +4945,19 @@ describe(Rundown.name, () => {
           const segment: Segment = EntityTestFactory.createSegment({ parts: [unplannedPartOne, unplannedPartTwo, onAirPart] })
           const threshold: number = 1
 
-          const testee: Rundown = new Rundown({ segments: [segment], mode: RundownMode.ACTIVE, alreadyActiveProperties: {
-            activeCursor: {
-              segment,
-              part: onAirPart
+          const testee: Rundown = new Rundown(EntityTestFactory.createRundownInterface({
+            segments: [segment],
+            mode: RundownMode.ACTIVE,
+            alreadyActiveProperties: {
+              activeCursor: {
+                segment,
+                part: onAirPart,
+                owner: Owner.SYSTEM,
+              },
+              nextCursor: undefined,
+              infinitePieces: new Map(),
             }
-          } } as RundownInterface)
+          }))
 
           const result: string[] = testee.pruneOldUnplannedPartsOnActiveSegment(threshold)
           expect(result).toHaveLength(2)
@@ -4955,28 +4969,34 @@ describe(Rundown.name, () => {
   })
 
   describe(Rundown.prototype.stopPiece.name, () => {
-    describe('the Rundown isn\'t active', () => {
+    describe('the rundown is inactive', () => {
       it('throws NotActivateException', () => {
         const pieceId: string = 'randomPieceId'
-        const testee: Rundown = new Rundown({ mode: RundownMode.INACTIVE } as RundownInterface)
+        const testee: Rundown = new Rundown(EntityTestFactory.createRundownInterface({ mode: RundownMode.INACTIVE }))
 
         expect(() => testee.stopPiece(pieceId)).toThrow(NotActivatedException)
       })
     })
 
-    describe('the Rundown is active', () => {
+    describe('the rundown is active', () => {
       describe('the Piece is on the Active Part', () => {
         it ('stops the Piece', () => {
           const piece: Piece = EntityTestFactory.createPiece({ id: 'pieceId', executedAt: 12345678, duration: undefined })
           const activePart: Part = EntityTestFactory.createPart({ id: 'activePart', pieces: [piece] })
           const segment: Segment = EntityTestFactory.createSegment({ id: 'segment', parts: [activePart] })
 
-          const testee: Rundown = new Rundown({ mode: RundownMode.ACTIVE, alreadyActiveProperties: {
-            activeCursor: {
-              part: activePart,
-              segment
+          const testee: Rundown = new Rundown(EntityTestFactory.createRundownInterface({
+            mode: RundownMode.ACTIVE,
+            alreadyActiveProperties: {
+              activeCursor: {
+                part: activePart,
+                segment,
+                owner: Owner.SYSTEM,
+              },
+              nextCursor: undefined,
+              infinitePieces: new Map(),
             }
-          }} as RundownInterface)
+          }))
 
           expect(piece.hasEnded(Date.now())).toBeFalsy()
           testee.stopPiece(piece.id)
@@ -4993,13 +5013,18 @@ describe(Rundown.name, () => {
           const activePart: Part = EntityTestFactory.createPart({ id: 'activePart' })
           const segment: Segment = EntityTestFactory.createSegment({ id: 'segment', parts: [activePart] })
 
-          const testee: Rundown = new Rundown({ mode: RundownMode.ACTIVE, alreadyActiveProperties: {
-            activeCursor: {
-              part: activePart,
-              segment
-            },
-            infinitePieces
-          }} as RundownInterface)
+          const testee: Rundown = new Rundown(EntityTestFactory.createRundownInterface({
+            mode: RundownMode.ACTIVE,
+            alreadyActiveProperties: {
+              activeCursor: {
+                part: activePart,
+                segment,
+                owner: Owner.SYSTEM,
+              },
+              nextCursor: undefined,
+              infinitePieces
+            }
+          }))
 
           expect(infinitePiece.hasEnded(Date.now())).toBeFalsy()
           testee.stopPiece(infinitePiece.id)
