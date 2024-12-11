69780e06c675d848cfa46dd427cdb915b33a763b
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2024-12-11 14:09:46 +0100

    Add missing attributes required on PieceDto

--- src/presentation/dtos/piece-dto.ts
+++ src/presentation/dtos/piece-dto.ts
@@ -4,23 +4,27 @@ import { PieceLifespan } from '../../model/enums/piece-lifespan'
 export class PieceDto {
   public readonly id: string
   public readonly partId: string
+  public readonly rundownId: string
   public readonly name: string
   public readonly start: number
   public readonly duration?: number
   public readonly layer: string
   public readonly isPlanned: boolean
   public readonly lifespan: PieceLifespan
+  public readonly executedAt: number
   public readonly metadata?: unknown
 
   constructor(piece: Piece) {
     this.id = piece.id
     this.partId = piece.getPartId()
+    this.rundownId = piece.rundownId
     this.name = piece.name
     this.start = piece.getStart()
     this.duration = piece.getDuration()
     this.layer = piece.layer
     this.isPlanned = piece.isPlanned
     this.lifespan = piece.pieceLifespan
+    this.executedAt = piece.getExecutedAt()
     this.metadata = piece.metadata
   }
 }
