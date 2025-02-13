fdbce838600de072b26a57c240af5d4bac654cca
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2025-02-13 13:46:18 +0100

    SOF-2477 Fix build issues from moving PieceMetadata

--- src/blueprints/tv2/entities/tv2-piece-interface.ts
+++ src/blueprints/tv2/entities/tv2-piece-interface.ts
@@ -1,6 +1,6 @@
 import { PieceInterface } from '../../../model/entities/piece'
-import { Tv2PieceMetadata } from '../value-objects/tv2-blueprint-timeline-object'
+import { PieceMetadata } from '../../../model/value-objects/metadata'
 
 export interface Tv2PieceInterface extends PieceInterface {
-  metadata: Tv2PieceMetadata
+  metadata: PieceMetadata
 }
--- src/blueprints/tv2/value-objects/tv2-part-end-state.ts
+++ src/blueprints/tv2/value-objects/tv2-part-end-state.ts
@@ -1,8 +1,8 @@
-import { Tv2SisyfosPersistenceMetadata } from './tv2-blueprint-timeline-object'
+import { SisyfosPersistenceMetadata } from '../../../model/value-objects/metadata'
 
 // These values are taken from "PartEndStateExt" in Blueprints
 export interface Tv2PartEndState {
-  sisyfosPersistenceMetadata: Tv2SisyfosPersistenceMetadata
+  sisyfosPersistenceMetadata: SisyfosPersistenceMetadata
   isJingle?: boolean
   fullFileName?: string
   serverPosition?: Tv2ServerPosition
