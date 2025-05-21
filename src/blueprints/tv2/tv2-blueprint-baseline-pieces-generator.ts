715c2cb7b109531edf4a54c9aedcd51ca829d4d1
vushu <davu@tv2.dk>
2025-05-21 12:38:29 +0200

    Updated with takenOffAirTimestamp

--- src/blueprints/tv2/tv2-blueprint-baseline-pieces-generator.ts
+++ src/blueprints/tv2/tv2-blueprint-baseline-pieces-generator.ts
@@ -48,9 +48,9 @@ export class Tv2BlueprintBaselinePiecesGenerator implements BlueprintBaselinePie
           isPlanned: true,
           isUnsynced: false,
           start: 0,
-          takenOffAirTimestamp: 0,
           preRollDuration: 0,
           postRollDuration: 0,
+          takenOffAirTimestamp: 0,
           timelineObjects: [
             this.videoMixerTimelineObjectFactory.createDownstreamKeyerTimelineObject(downstreamKeyer, downstreamKeyer.defaultOn)
           ],
