5715177008e252dbd0cf341efff416c9c59e2e9d
Anders Frederik Lou Jørgensen <afjo@tv2.dk>
2025-06-11 09:55:41 +0200

    refactor: Adds TODO for moving shelf configuration to Alba TV 2 server.

--- src/data-access/repositories/interfaces/shelf-configuration-repository.ts
+++ src/data-access/repositories/interfaces/shelf-configuration-repository.ts
@@ -1,5 +1,6 @@
 import { ShelfConfiguration } from '../../../rundown-execution/domain/entities/shelf-configuration'
 
+// TODO: Move to Alba TV 2 server
 export interface ShelfConfigurationRepository {
   getShelfConfiguration(): Promise<ShelfConfiguration>
   updateShelfConfiguration(shelfConfiguration: ShelfConfiguration): Promise<ShelfConfiguration>
