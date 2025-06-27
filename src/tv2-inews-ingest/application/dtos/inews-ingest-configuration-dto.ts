18f02b42d31f3087ebdb1515c6b84ca3e82ef7fc
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2025-06-27 07:54:46 +0200

    AG-390 Move InewsQueueSubscription to seperate file

--- src/tv2-inews-ingest/application/dtos/inews-ingest-configuration-dto.ts
+++ src/tv2-inews-ingest/application/dtos/inews-ingest-configuration-dto.ts
@@ -1,19 +1,10 @@
-import { InewsIngestConfiguration, InewsQueueSubscription } from '../../domain/entities/inews-ingest-configuration'
+import { InewsIngestConfiguration } from '../../domain/entities/inews-ingest-configuration'
+import { InewsQueueSubscriptionDto } from './inews-queue-subscription-dto'
 
 export class InewsIngestConfigurationDto {
   public queueSubscriptions: InewsQueueSubscriptionDto[]
 
   public constructor(inewsIngestConfiguration: InewsIngestConfiguration) {
     this.queueSubscriptions = inewsIngestConfiguration.queueSubscriptions.map(subscription => new InewsQueueSubscriptionDto(subscription))
-  }
-}
-
-class InewsQueueSubscriptionDto {
-  public queueId: string
-  public isDisabled: boolean
-
-  public constructor(inewsQueueSubscription: InewsQueueSubscription) {
-    this.queueId = inewsQueueSubscription.queueId
-    this.isDisabled = inewsQueueSubscription.isDisabled
   }
 }
--- src/tv2-inews-ingest/domain/entities/inews-ingest-configuration.ts
+++ src/tv2-inews-ingest/domain/entities/inews-ingest-configuration.ts
@@ -1,8 +1,5 @@
+import { InewsQueueSubscription } from '../value-objects/ines-queue-subscription'
+
 export interface InewsIngestConfiguration {
   queueSubscriptions: InewsQueueSubscription[]
-}
-
-export interface InewsQueueSubscription {
-  queueId: string
-  isDisabled: boolean
 }
