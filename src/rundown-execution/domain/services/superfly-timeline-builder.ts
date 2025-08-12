b3d08b25445e6d4f0bc09f6ad20c9e1eda987870
Rasmus <raal@tv2.dk>
2025-08-12 09:42:39 +0200

    fix: Removes the changes that wasnt needed for lookahead

--- src/rundown-execution/domain/services/superfly-timeline-builder.ts
+++ src/rundown-execution/domain/services/superfly-timeline-builder.ts
@@ -441,7 +441,7 @@ export class SuperflyTimelineBuilder implements TimelineBuilder {
     }
     const activePartTimelineObjectEnable: TimelineEnable = {
       start: 0,
-      end: `#${activeGroup.id}`,
+      end: `#${activeGroup.id}.start`,
     }
     return this.createLookaheadTimelineObjectsForPart(
       rundown.getActivePart(),
