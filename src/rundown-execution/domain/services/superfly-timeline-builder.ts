3f248cc1616ffff074b11ac3a25e0f234c9ee5c9
Anders Frederik Lou Jørgensen <afjo@tv2.dk>
2025-09-05 13:18:11 +0200

    fix: Comments out infinite piece executedAt check in timeline builder.
    This is a bandaid solution and the infinite piece flow should be looked further
    into to fix the root cause and the check should be re-enabled.

--- src/rundown-execution/domain/services/superfly-timeline-builder.ts
+++ src/rundown-execution/domain/services/superfly-timeline-builder.ts
@@ -526,11 +526,13 @@ export class SuperflyTimelineBuilder implements TimelineBuilder {
       .filter(piece => piece.getPartId() !== activePart.id)
       .filter(piece => !piece.hasEnded(now))
       .forEach((piece) => {
-        if (!piece.getExecutedAt()) {
-          throw new UnsupportedOperationException(
-            `Found infinite Piece '${piece.name}' with id '${piece.id}' without an "executedAt". Infinite Pieces must have an "executedAt"! ${piece.pieceLifespan}`
-          )
-        }
+        // NOTE: This is commented out in AG-705.
+        //       Please have a look at how we handle infinite pieces to fix the root cause and re-enable this check.
+        // if (!piece.getExecutedAt()) {
+        //   throw new UnsupportedOperationException(
+        //     `Found infinite Piece '${piece.name}' with id '${piece.id}' without an "executedAt". Infinite Pieces must have an "executedAt"! ${piece.pieceLifespan}`
+        //   )
+        // }
 
         const infiniteGroup: TimelineObjectGroup = {
           id: `${INFINITE_GROUP_PREFIX}${activePart.id}_${piece.id}`,
